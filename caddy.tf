b2c7a3ca12aa21eb4c1344c8716d3c3ec2c08a2e
Daniel Corbett <dcorbett@haproxy.com>
2020-01-16 19:42:27 -0500

    Add Caddy 2

---
+++ caddy.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "caddy" {
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "${var.aws_instance_type}"
+  subnet_id              = "${aws_subnet.benchmarks.id}"
+  vpc_security_group_ids = ["${aws_security_group.benchmarks.id}"]
+  key_name               = "${var.ssh_keypair_name}"
+  depends_on             = ["aws_instance.webserver"]
+
+  tags = {
+    Name = "caddy"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${file("./${var.ssh_keypair_name}.pem")}"
+    host = "${self.public_ip}"
+  }
+
+  provisioner "file" {
+    source      = "./caddy/Caddyfile"
+    destination = "/tmp/Caddyfile"
+  }
+
+  provisioner "file" {
+    source      = "./caddy/caddy_setup.sh"
+    destination = "/home/ubuntu/setup.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /home/ubuntu/setup.sh",
+        "sudo /home/ubuntu/setup.sh",
+    ]
+  }
+}
+
+output "caddy_public_ip" {
+  value = "${aws_instance.caddy.public_ip}"
+}
