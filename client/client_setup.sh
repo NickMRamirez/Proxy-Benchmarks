33e99d3998e8f792680da53ebc7454f04d0b1dd8
Nick Ramirez <nickram44@gmail.com>
2021-03-07 12:34:13 -0500

    Added system tuning to increase VM capacity on client and proxy servers.

--- client/client_setup.sh
+++ client/client_setup.sh
@@ -4,3 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 # Install Hey
 wget -O hey https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_amd64
 chmod +x ./hey
+
+sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+sysctl -w net.ipv4.tcp_tw_reuse=1
+sysctl -w net.ipv4.tcp_timestamps=1
+ulimit -n 250000
--- docker/docker_setup.sh
+++ docker/docker_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+sysctl -w net.ipv4.tcp_tw_reuse=1
+sysctl -w net.ipv4.tcp_timestamps=1
+ulimit -n 250000
+
 # Install Docker
 if [ ! $(which docker) ]; then
     echo "----Installing docker----"
